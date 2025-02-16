a31412e841a0f08394ed6ccefc29d2473027fb0b
Oydar <oydar1@outlook.com>
2025-02-16 22:33:22 +0300

    add binary search

--- algorithms/search/binary.py
+++ algorithms/search/binary.py
@@ -1,10 +1,15 @@
 from typing import List
 
-def binary_search(unsorted_list: List[int], target: int) -> List[int]:
-
+def binary_search(unsorted_list: List[int], target: int, left: int = 0, right: int = 0) -> int:
     middle = unsorted_list[len(unsorted_list) // 2]
-    print(middle)
+
+    if middle == target:
+        return 0
+
+    return 1
+
 
 if __name__ == "__main__":
-    a = [3, 1, 4, 5, 23]
+    a = [3, 1, 5, 4, 23]
+    a.sort()
     binary_search(a, 5)
